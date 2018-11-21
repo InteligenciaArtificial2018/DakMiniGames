d6f168901328ad6a41cf4535d700f8fa67059e19
Dakaro15 <Dakaro_15@yahoo.com>
2018-11-21 08:53:27 -0600

    primer commit del proyecto.

---
+++ app/src/main/java/com/example/dakaro/dakminigames/Puntaje.kt
@@ -0,0 +1,19 @@
+package com.example.dakaro.dakminigames
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Puntaje : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_puntaje)
+
+        /*
+        var puntaje =
+        var tvPuntajito = findViewById<TextView>(R.id.tvPuntaje)
+
+        tvPuntajito =
+        */
+    }
+}
